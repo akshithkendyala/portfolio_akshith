2a6a8044ba39df284cc7673f1d0b59c71aa37025
akshithkendyala <akshithkendyala@gmail.com>
2026-07-18 22:27:53 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // If your site is hosted at username.github.io/repo-name, you also need:
   basePath: '/portfolio_akshith',
   images: {
-    unoptimized: true, // GitHub Pages doesn't support Next.js default image optimization
+    unoptimized: true,
   },
 };
 
