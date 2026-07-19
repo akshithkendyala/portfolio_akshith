e6557bacab10c4716e9124bc68a467a5969f0837
akshithkendyala <akshithkendyala@gmail.com>
2026-07-19 08:22:45 +0530

    Contact section update

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -9,7 +9,7 @@ export const developerProfile = {
   github: "https://github.com/akshithkendyala",
   linkedin: "https://www.linkedin.com/in/sai-akshith-kendyala-309205360/",
   resumeUrl: "https://drive.google.com/file/d/1d45dOfLyxNpgNqiJDMlRCepTlSbhJTNQ/view?usp=sharing", // Add resume download path or placeholder link
-  web3formsKey: "YOUR_WEB3FORMS_ACCESS_KEY", // Request a free key at https://web3forms.com
+  web3formsKey: "099631a1-9534-4f9d-85e9-3b5fc4078ab0", // Request a free key at https://web3forms.com
 };
 
 export const codingProfiles = [
